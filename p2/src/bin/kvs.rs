547ceadfa3f6b99a2be869c1bddc1085b3df692b
Fan Gao <gaofan404@gmail.com>
2020-06-16 22:03:40 -0500

    init p2

---
+++ p2/src/bin/kvs.rs
@@ -0,0 +1,44 @@
+// #[allow(dead_code)]
+use clap::{App, Arg, SubCommand};
+use std::process::exit;
+
+/// KvStore stores <key, value> pairs in a HashMap.
+fn main() {
+    let matches = App::new("kvs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set <k, v> pair")
+                .arg(Arg::with_name("KEY").required(true))
+                .arg(Arg::with_name("VALUE").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get <k, v> pair")
+                .arg(Arg::with_name("KEY").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Remove <k, v> pair")
+                .arg(Arg::with_name("KEY").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("set", _) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("get", _) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        ("rm", _) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => panic!(),
+    }
+}
